a1dc505034dbf0eb8d840580e28c34d1d3c140a4
flavioshinzato <f.a.m.shinzato@gmail.com>
2017-03-16 21:57:37 -0400

    Criação da classe Torta

---
+++ model/torta.php
@@ -0,0 +1,40 @@
+<?php
+
+class Torta {
+	
+	private $nome;
+	private $precoP;
+	private $precoM;
+	private $precoG;
+	private $desc;
+
+	public function Torta($nome, $precoP, $precoM, $precoM, $desc) {
+		$this->nome = $nome;
+		$this->precoP = $precoP;
+		$this->precoM = $precoM;
+		$this->precoG = $precoG;
+		$this->desc = $desc;
+	}
+
+	public function getNome() {
+		return $this->nome;
+	}
+
+	public function getNome() {
+		return $this->precoP;
+	}
+
+	public function getPrecoM() {
+		return $this->precoM;
+	}
+
+	public function getPrecoG() {
+		return $this->precoG;
+	}
+
+	public function getDesc() {
+		return $this->desc;
+	}
+}
+
+?>
