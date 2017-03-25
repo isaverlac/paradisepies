dd0a0c90fa6b39d034ac653858bf55a24a63f989
Isabela Andrade <isaverlac@gmail.com>
2017-03-25 02:12:33 -0400

    adicionando /view nos diretorios

--- view/mensagem.php
+++ view/mensagem.php
@@ -78,7 +78,7 @@ $('a.poplight[href^=#]').click(function() {
     var dim= query[1].split('&');
     var popWidth = dim[0].split('=')[1]; //Gets the first query string value
     //Fade in the Popup and add close button
-    $('#' + popID).fadeIn().css({ 'width': Number( popWidth ) }).prepend('<a href="#" class="close"><img src="Imagens/close.png" class="btn_close" title="Close Window" alt="Close" /></a>');
+    $('#' + popID).fadeIn().css({ 'width': Number( popWidth ) }).prepend('<a href="#" class="close"><img src="view/Imagens/close.png" class="btn_close" title="Close Window" alt="Close" /></a>');
     //Define margin for center alignment (vertical   horizontal) - we add 80px to the height/width to accomodate for the padding  and border width defined in the css
     var popMargTop = ($('#' + popID).height() + 80) / 2;
     var popMargLeft = ($('#' + popID).width() + 80) / 2;
@@ -118,7 +118,7 @@ src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
 
 <div id="sticky-anchor"></div>
 <nav>
-	<a href="cadastroUsuario.html"> Voltar </a>
+	<a href="view/cadastroUsuario.html"> Voltar </a>
 </nav>
 
 
@@ -142,16 +142,16 @@ src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
 				<h2>Onde nos encontrar</h2>
 
 			
-					<a href="https://www.facebook.com/paradisepie/"> <img src="Social/facebook.png" alt="facebook"/> curta nossa página </a>
-					<a href="https://www.instagram.com/paradise.pies/"> <img src="Social/instagram.png" alt="instagram"/> nos siga no instagram  </a>
+					<a href="https://www.facebook.com/paradisepie/"> <img src="view/Social/facebook.png" alt="facebook"/> curta nossa página </a>
+					<a href="https://www.instagram.com/paradise.pies/"> <img src="view/Social/instagram.png" alt="instagram"/> nos siga no instagram  </a>
 					
 					<br>
 					 
-					 <img src="Social/home.png" alt="Endereço"/> Rua 13 de Maio, 1220, Campo Grande - MS 
+					 <img src="view/Social/home.png" alt="Endereço"/> Rua 13 de Maio, 1220, Campo Grande - MS 
 					 
 					 <br>
 					 
-					 <img src="Social/email.png" alt="Email"/> paradisepies@gmail.com 
+					 <img src="view/Social/email.png" alt="Email"/> paradisepies@gmail.com 
 			
 				
 			</div>
@@ -162,7 +162,7 @@ src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
 			
 			<div class="caixaSobreMim">
 			  
-			  <img src="Imagens/me.jpg" class="portrait" alt="Sobre Mim"/> 
+			  <img src="view/Imagens/me.jpg" class="portrait" alt="Sobre Mim"/> 
 			  
 			  Meu nome é Isabela, tenho 20 anos. Sou apaixonada por doces e por web design. Faço ambos com muito carinho, gosto de agradar as pessoas com as
 			  coisas que eu sou boa. Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá Blá 
@@ -171,10 +171,10 @@ src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
 			
 		
 			
-			<a href="https://www.facebook.com/isaverlac/"><img src="Social/facebook.png" alt="facebook"/></a>
-			<a href="https://twitter.com/isaverlac"><img src="Social/instagram.png" alt="instagram"/></a>
-			<a href="https://www.instagram.com/isaverlac/"><img src="Social/twitter.png" alt="twitter"/></a>
-			<a href="https://city0fglass.tumblr.com"><img src="Social/tumblr.png" alt="tumblr"/></a>
+			<a href="https://www.facebook.com/isaverlac/"><img src="view/Social/facebook.png" alt="facebook"/></a>
+			<a href="https://twitter.com/isaverlac"><img src="view/Social/instagram.png" alt="instagram"/></a>
+			<a href="https://www.instagram.com/isaverlac/"><img src="view/Social/twitter.png" alt="twitter"/></a>
+			<a href="https://city0fglass.tumblr.com"><img src="view/Social/tumblr.png" alt="tumblr"/></a>
 			
 			</div>
 	
